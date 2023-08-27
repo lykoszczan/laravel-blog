25ae33eb0b883d04b8916556591cf4cc6492005b
Marcin Lykowski <mlykowski@leonsoftware.com>
2023-08-27 22:18:10 +0200

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -8,16 +8,21 @@ use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [
-    RegistrationController::class,
-    'store',
-]);
-Route::post('login', [
-    LoginController::class,
-    'login',
+Route::get('posts', [
+    PostController::class,
+    'index',
 ]);
 
 Route::middleware(['guest'])->group(function () {
+    Route::post('register', [
+        RegistrationController::class,
+        'store',
+    ]);
+    Route::post('login', [
+        LoginController::class,
+        'login',
+    ]);
+
     Route::post('forgot-password', [
         PasswordResetController::class,
         'sendResetToken',
@@ -28,11 +33,6 @@ Route::middleware(['guest'])->group(function () {
         'resetPassword',
     ]);
 });
-
-Route::get('posts', [
-    PostController::class,
-    'index',
-]);
 
 Route::middleware(['auth:api'])->group(function () {
     Route::post('logout', [
